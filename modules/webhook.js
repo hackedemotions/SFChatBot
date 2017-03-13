2c0c0441aa6d427c0883fd9414bbc8878d3f7e47
abkumar <abkumar@coh.org>
2017-03-13 15:47:51 -0700

    Updated Code

--- modules/webhook.js
+++ modules/webhook.js
@@ -7,10 +7,10 @@ let request = require('request'),
 let sendMessage = (message, recipient) => {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.FB_PAGE_TOKEN},
+        qs: { access_token: process.env.FB_PAGE_TOKEN },
         method: 'POST',
         json: {
-            recipient: {id: recipient},
+            recipient: { id: recipient },
             message: message
         }
     }, (error, response) => {
@@ -22,24 +22,26 @@ let sendMessage = (message, recipient) => {
     });
 };
 
-let processText = (text, sender)  => {
+let processText = (text, sender) => {
     let match;
     match = text.match(/hi/i);
     if (match) {
         sendMessage({
-        text:
-        `Hi, How are you doing today ?:
+            text:
+            `Hi, How are you doing today ?:
             How can I help You ?
     `}, sender);
-    return;
+        return;
     }
 
     match = text.match(/help/i);
     if (match) {
-        sendMessage({text:
+        sendMessage({
+            text:
             `You can ask me things like:
     Search patient <<name>>
-    Search appointment <<date>>
+    Search doctor <<name>>
+    Search appt <<date>>
     Create case <<summary>>
         `}, sender);
         return;
@@ -48,39 +50,57 @@ let processText = (text, sender)  => {
 
     match = text.match(/search patient (.*)/i);
     if (match) {
-        sendMessage({ text: `Searching for the patient ${match[1]}":` }, sender);
+        sendMessage({ text: `Searching for patient "${match[1]}":` }, sender);
         salesforce.findAccount(match[1]).then(accounts => {
-            sendMessage({text: `Here are the accounts I found matching "${match[1]}":`}, sender);
+            sendMessage({ text: `Here are the accounts I found matching "${match[1]}":` }, sender);
             sendMessage(formatter.formatAccounts(accounts), sender)
         });
         return;
     }
     else {
-        sendMessage({ text: `No match Found` }, sender);
+        sendMessage({
+            text: `Sorry, I canont understand.
+        You can ask me things like:
+        Search patient <<name>>
+        Search doctor <<name>>
+        Search appt <<date>>
+        Create case <<summary>>
+        ` }, sender);
     }
 
     match = text.match(/search (.*) in accounts/i);
     if (match) {
         salesforce.findAccount(match[1]).then(accounts => {
-            sendMessage({text: `Here are the accounts I found matching "${match[1]}":`}, sender);
+            sendMessage({ text: `Here are the accounts I found matching "${match[1]}":` }, sender);
             sendMessage(formatter.formatAccounts(accounts), sender)
         });
         return;
     }
 
-    match = text.match(/search a doctor (.*)/i);
+    match = text.match(/search doctor (.*)/i);
     if (match) {
+        sendMessage({ text: `Searching for doctor "${match[1]}":` }, sender);
         salesforce.findContact(match[1]).then(contacts => {
-            sendMessage({text: `Here are the contacts I found matching "${match[1]}":`}, sender);
+            sendMessage({ text: `Here are the contacts I found matching "${match[1]}":` }, sender);
             sendMessage(formatter.formatContacts(contacts), sender)
         });
         return;
+    }
+    else {
+        sendMessage({
+            text: `Sorry, I canont understand.
+        You can ask me things like:
+        Search patient <<name>>
+        Search doctor <<name>>
+        Search appt <<date>>
+        Create case <<summary>>
+        ` }, sender);
     }
 
     match = text.match(/top (.*) opportunities/i);
     if (match) {
         salesforce.getTopOpportunities(match[1]).then(opportunities => {
-            sendMessage({text: `Here are your top ${match[1]} opportunities:`}, sender);
+            sendMessage({ text: `Here are your top ${match[1]} opportunities:` }, sender);
             sendMessage(formatter.formatOpportunities(opportunities), sender)
         });
         return;
@@ -100,18 +120,18 @@ let handlePost = (req, res) => {
         let event = events[i];
         let sender = event.sender.id;
         if (process.env.MAINTENANCE_MODE && ((event.message && event.message.text) || event.postback)) {
-            sendMessage({text: `Sorry I'm taking a break right now.`}, sender);
+            sendMessage({ text: `Sorry I'm taking a break right now.` }, sender);
         } else if (event.message && event.message.text) {
             processText(event.message.text, sender);
         } else if (event.postback) {
             let payload = event.postback.payload.split(",");
             if (payload[0] === "view_contacts") {
-                sendMessage({text: "OK, looking for your contacts at " + payload[2] + "..."}, sender);
+                sendMessage({ text: "OK, looking for your contacts at " + payload[2] + "..." }, sender);
                 salesforce.findContactsByAccount(payload[1]).then(contacts => sendMessage(formatter.formatContacts(contacts), sender));
             } else if (payload[0] === "close_won") {
-                sendMessage({text: `OK, I closed the opportunity "${payload[2]}" as "Close Won". Way to go Christophe!`}, sender);
+                sendMessage({ text: `OK, I closed the opportunity "${payload[2]}" as "Close Won". Way to go Christophe!` }, sender);
             } else if (payload[0] === "close_lost") {
-                sendMessage({text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".`}, sender);
+                sendMessage({ text: `I'm sorry to hear that. I closed the opportunity "${payload[2]}" as "Close Lost".` }, sender);
             }
         }
     }
