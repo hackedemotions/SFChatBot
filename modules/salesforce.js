39cd852e4d9f492a1144362cdcb42deb3c2e9498
abkumar <abkumar@coh.org>
2017-03-14 12:24:30 -0700

    Removed sandbox

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -11,7 +11,6 @@ let org = nforce.createConnection({
     clientId: SF_CLIENT_ID,
     clientSecret: SF_CLIENT_SECRET,
     redirectUri: 'http://localhost:3000/oauth/_callback',
-    environment: 'sandbox',
     mode: 'single',
     autoRefresh: true
 });
