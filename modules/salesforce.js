06ca436215f3da0f48903a2a3bf5acf423044fc9
abkumar <abkumar@coh.org>
2017-03-14 19:00:53 -0700

    Updated code for Sandbox

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -11,6 +11,7 @@ let org = nforce.createConnection({
     clientId: SF_CLIENT_ID,
     clientSecret: SF_CLIENT_SECRET,
     redirectUri: 'https://sfchatbottry1.herokuapp.com/oauth/_callback',
+    environment: 'sandbox',
     mode: 'single',
     autoRefresh: true
 });
